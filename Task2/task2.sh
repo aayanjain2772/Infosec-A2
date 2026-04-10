8b52105317b1b08e21730bdc0a232256f5d1d5a9
aayanjain2772 <aayan.jain_ug2023@ashoka.edu.in>
2026-04-10 15:59:58 +0530

    implemented generating public and privte keys

--- Task2/task2.sh
+++ Task2/task2.sh
@@ -1,12 +1,12 @@
-# Generate a recipient RSA key pair
+# Generating a recipient RSA key pair
 openssl genrsa -out recipient_private_key.pem 2048
 openssl rsa -in recipient_private_key.pem -pubout -out recipient_public_key.pem
 
-# Generate a random 256-bit symmetric key (binary)
+# Generating a random 256-bit symmetric key (binary)
 openssl rand -out symmetric_key.bin 32
 echo "Generated random 256-bit symmetric key (symmetric_key.bin)"
 
-# Encrypt the symmetric key using the recipient's RSA public key
+# Encrypting the symmetric key using the recipient's RSA public key
 openssl rsautl -encrypt -inkey recipient_public_key.pem -pubin \
   -in symmetric_key.bin -out encrypted_symmetric_key.bin
 echo "Symmetric key encrypted with recipient's public key."
@@ -16,13 +16,13 @@ openssl rsautl -decrypt -inkey recipient_private_key.pem \
   -in encrypted_symmetric_key.bin -out decrypted_symmetric_key.bin
 echo "Recipient decrypted the symmetric key successfully."
 
-# Encrypt a message using the shared symmetric key (AES-256-CBC)
+# Encrypting a message using the shared symmetric key (AES-256-CBC)
 echo "This is a confidential message protected by the shared key." > message.txt
 openssl enc -aes-256-cbc -kfile symmetric_key.bin \
   -in message.txt -out encrypted_message.bin -pbkdf2
 echo "Message encrypted using symmetric key."
 
-# Decrypt the message using the decrypted symmetric key
+# Decrypting the message using the decrypted symmetric key
 openssl enc -d -aes-256-cbc -kfile decrypted_symmetric_key.bin \
   -in encrypted_message.bin -out decrypted_message.txt -pbkdf2
 echo "Decrypted message:"
