d50c2b375f92b79610a505c73553a8eac65bf2f6
aayanjain2772 <aayan.jain_ug2023@ashoka.edu.in>
2026-04-10 15:51:20 +0530

    changes done

--- Task1/task1.sh
+++ Task1/task1.sh
@@ -1,20 +1,20 @@
-# Generate a 2048-bit RSA private key
+# Generating a 2048-bit RSA private key
 openssl genrsa -out private_key.pem 2048
 
-#  Extract the corresponding public key
+#  Extracting the corresponding public key
 openssl rsa -in private_key.pem -pubout -out public_key.pem
 
-# Create the message to encrypt
+# Creating the message to encrypt
 echo "Hello, this is a secret message." > message.txt
 
-#  Encrypt the message using the public key (RSA PKCS#1 v1.5)
+#  Encrypting the message using the public key (RSA PKCS#1 v1.5)
 openssl rsautl -encrypt -inkey public_key.pem -pubin \
   -in message.txt -out encrypted_message.bin
 
-# Decrypt the message using the private key
+# Decrypting the message using the private key
 openssl rsautl -decrypt -inkey private_key.pem \
   -in encrypted_message.bin -out decrypted_message.txt
 
-# Verify decryption
+# Verifying the decryption done
 echo "Decrypted message:"
 cat decrypted_message.txt
